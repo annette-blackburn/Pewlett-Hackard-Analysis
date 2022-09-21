657bd6a2276b5f018d8a6002fe5532b7039dc6d5
Annette Donald Blackburn <109121304+annette-blackburn@users.noreply.github.com>
2022-09-21 16:09:20 -0600

    Add files via upload

--- schema.sql
+++ schema.sql
@@ -12,17 +12,18 @@ CREATE TABLE employees (
 	first_name VARCHAR NOT NULL,
 	last_name VARCHAR NOT NULL,
 	gender VARCHAR NOT NULL,
-	hire_date DATE NOT NULL.
+	hire_date DATE NOT NULL,
 	PRIMARY KEY (emp_no)
 );
 
-CREATE TABLE dept_manager(
+CREATE TABLE dept_manager (
 dept_no VARCHAR(4) NOT NULL,
 	emp_no INT NOT NULL,
 	from_date DATE NOT NULL,
-	to_date NOT NULL,
-	FOREIGN KEY (emp_no) REFRENCES employees (emp_no)
-	FOREIGN KEY (dept_no) REFRENCES departments (dept_no)
+	to_date DATE NOT NULL,
+FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+	PRIMARY KEY (emp_no, dept_no)
 );
 
 CREATE TABLE salaries (
@@ -37,17 +38,14 @@ CREATE TABLE salaries (
 CREATE TABLE dept_employees (
 	emp_no VARCHAR NOT NULL,
 	dept_no VARCHAR NOT NULL,
-	from_date INT NOT NULL,
-	to_date INT NOT NULL,
-	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	FOREIGN KEY (dept_no) REFRENCES departments (dept_no)
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL
 );
 
 
 CREATE TABLE titles (
 	emp_no VARCHAR NOT NULL,
 	salary INT NOT NULL,
-	from_date INT NOT NULL,
-	to_date INT NOT NULL,
-	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL
 );
